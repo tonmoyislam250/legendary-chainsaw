daeac407b993538938b9e73e66181783d6d69059
tonmoyislam250 <su6087031@gmail.com>
2022-09-22 23:04:02 +0600

    JJ

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Repo", "https://github.com/tonmoyislam250/legendary-chainsaw")
+    buttons.buildbutton("Report Group", "https://t.me/+j6W32ofEDithODI1")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
