c327cd740a17221461c46d06859f662c5defb267
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2022-11-28 19:00:05 +0600

    Update pewdiepie.py

--- pewdiepie.py
+++ pewdiepie.py
@@ -1,7 +1,7 @@
 from subprocess import run as srun
 
 UPSTREAM_REPO="https://github.com/tonmoyislam250/legendary-chainsaw"
-UPSTREAM_BRANCH="pulled"
+UPSTREAM_BRANCH="anasty"
 
 srun([f"git init -q \
 && git config --global user.email su6087031@gmail.com \
