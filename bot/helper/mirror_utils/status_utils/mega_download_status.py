b3b3bd03f504a9dfa040a8f7d7832ac484f05913
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2023-02-20 23:31:15 +0600

    Update mega_download_status.py

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -1,8 +1,8 @@
-import os
+import os,subprocess
 from bot import DOWNLOAD_DIR
 from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time
 
-engine_= os.popen('megasdkrest -v').read()
+engine_= subprocess.getoutput('megasdkrest -v')
 class MegaDownloadStatus:
     def __init__(self, obj, listener):
         self.__obj = obj
