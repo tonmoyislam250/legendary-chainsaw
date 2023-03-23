609add4fa9a2995da38ee3cf7d4a7d14858fd5a7
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2023-03-23 12:51:48 +0600

    Update zip_status.py

--- bot/helper/mirror_utils/status_utils/zip_status.py
+++ bot/helper/mirror_utils/status_utils/zip_status.py
@@ -2,7 +2,7 @@ from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus
 
 
 class ZipStatus:
-    def __init__(self, name, path, size):
+    def __init__(self, name, path, size, listener):
         self.__name = name
         self.__path = path
         self.__size = size
