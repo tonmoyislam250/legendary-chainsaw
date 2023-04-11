ad304dc2ec3cbd05c9b5c99463f9382d32a956c2
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2023-04-12 00:26:38 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -21,7 +21,7 @@ PAGE_NO = 1
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['▏', '▎', '▍', '▌', '▋', '▊', '▉']
+PROGRESS_INCOMPLETE = ['▂','▃','▄','▅','▆','▇','█']
 
 class MirrorStatus:
     STATUS_UPLOADING = "Uploading...📤"
@@ -113,7 +113,7 @@ def get_progress_bar_string(status):
     p_str = '█' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
+    p_str += '░' * (PROGRESS_MAX_SIZE - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
