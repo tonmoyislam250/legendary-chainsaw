59a428ea70ef725dc137297f39364cda991f488c
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2023-05-15 18:21:34 +0600

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -1,13 +1,15 @@
-from os import path as ospath
-from aiofiles.os import remove as aioremove, path as aiopath, mkdir
-from time import time
-from re import search as re_search
 from asyncio import create_subprocess_exec
 from asyncio.subprocess import PIPE
+from os import path as ospath
+from re import search as re_search
+from time import time
+
+from aiofiles.os import mkdir
+from aiofiles.os import path as aiopath
+from aiofiles.os import remove as aioremove
 
 from bot import LOGGER, MAX_SPLIT_SIZE, config_dict, user_data
-from bot.helper.ext_utils.bot_utils import cmd_exec
-from bot.helper.ext_utils.bot_utils import sync_to_async
+from bot.helper.ext_utils.bot_utils import cmd_exec, sync_to_async
 from bot.helper.ext_utils.fs_utils import ARCH_EXT, get_mime_type
 
 
@@ -117,6 +119,7 @@ async def split_file(path, size, file_, dirpath, split_size, listener, start_tim
     user_dict = user_data.get(user_id, {})
     leech_split_size = user_dict.get(
         'split_size') or config_dict['LEECH_SPLIT_SIZE']
+    leech_split_size = min(leech_split_size, MAX_SPLIT_SIZE)
     parts = -(-size // leech_split_size)
     if (user_dict.get('equal_splits') or config_dict['EQUAL_SPLITS']) and not inLoop:
         split_size = ((size + parts - 1) // parts) + 1000
