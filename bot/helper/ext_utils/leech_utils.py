32cb40887058a46a4b65c540aaec17563c62773e
Tonmoy Islam <101395872+tonmoyislam250@users.noreply.github.com>
2023-05-15 18:48:54 +0600

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -127,11 +127,11 @@ async def split_file(path, size, file_, dirpath, split_size, listener, start_tim
             parted_name = f"{str(base_name)}.part{str(i).zfill(3)}{str(extension)}"
             out_path = ospath.join(dirpath, parted_name)
             if not noMap:
-                listener.suproc = Popen(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
+                listener.suproc = Popen(["mutahar", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
                                          "-i", path, "-fs", str(split_size), "-map", "0", "-map_chapters", "-1",
                                          "-c", "copy", out_path])
             else:
-                listener.suproc = Popen(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
+                listener.suproc = Popen(["mutahar", "-hide_banner", "-loglevel", "error", "-ss", str(start_time),
                                           "-i", path, "-fs", str(split_size), "-map_chapters", "-1", "-c", "copy",
                                           out_path])
             listener.suproc.wait()
