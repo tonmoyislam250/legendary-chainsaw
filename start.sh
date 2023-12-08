0ba4ddbc3990d01bf4ed65c1b3e2301bb525d35e
Tonmoy Islam <tonmoyk983@gmail.com>
2023-12-08 12:45:32 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
-python3 -c "import sys; sys.path.insert(0, '/usr/src/binary'); import pewdiepie"
+export CONFIG_FILE_URL="https://gist.githubusercontent.com/tonmoyislam>
+export UPSTREAM_REPO="https://github.com/tonmoyislam250/legendary-chai>
+export UPSTREAM_BRANCH="jmdkh"
+rm -rf .git
+python3 -c "import sys; sys.path.insert(0, '/usr/src/binary'); import >
 python3 -m bot
