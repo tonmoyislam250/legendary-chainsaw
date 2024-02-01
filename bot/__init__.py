dc21a8ea31293ffed2cb5a0197575608a89ec08d
Tonmoy Islam <tonmoyk983@gmail.com>
2024-02-01 12:53:51 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -305,8 +305,9 @@ EQUAL_SPLITS = EQUAL_SPLITS.lower() == 'true'
 MEDIA_GROUP = environ.get('MEDIA_GROUP', '')
 MEDIA_GROUP = MEDIA_GROUP.lower() == 'true'
 
-BASE_URL_PORT = environ.get('PORT')
+BASE_URL_PORT = environ.get('BASE_URL_PORT', '')
 BASE_URL_PORT = 80 if len(BASE_URL_PORT) == 0 else int(BASE_URL_PORT)
+
 
 BASE_URL = environ.get('BASE_URL', '').rstrip("/")
 if len(BASE_URL) == 0:
