3008de09563757481ab7f82020703a1c13088b82
Tonmoy Islam <tonmoyk983@gmail.com>
2024-02-03 11:27:39 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@ export UPSTREAM_REPO="https://github.com/tonmoyislam250/legendary-chainsaw"
 export UPSTREAM_BRANCH="jmdkh"
 rm -rf .git
 python3 -c "import sys; sys.path.insert(0, '/usr/src/binary'); import pewdiepie"
-pip install -r requirements.txt
 python3 -m bot
