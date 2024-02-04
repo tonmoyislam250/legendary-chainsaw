63f6a743d152ffaf0dadcf5f2e298960c7698682
Tonmoy Islam <tonmoyk983@gmail.com>
2024-02-04 12:50:06 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-export CONFIG_FILE_URL="https://gist.githubusercontent.com/tonmoyislam250/289777beb558ea473bb62f1f379fb869/raw/gistfile1.txt"
-export UPSTREAM_REPO="https://github.com/tonmoyislam250/legendary-chainsaw"
-export UPSTREAM_BRANCH="jmdkh"
 rm -rf .git
 python3 -c "import sys; sys.path.insert(0, '/usr/src/binary'); import pewdiepie"
 python3 -m bot
