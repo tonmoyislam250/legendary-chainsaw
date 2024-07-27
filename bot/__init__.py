841bae2f21861aa7189cb6a31180b9b0ac435682
tonmoyislam250 <tonmoyk983@gmail.com>
2024-07-27 12:12:15 +0600

    HH

--- bot/__init__.py
+++ bot/__init__.py
@@ -47,6 +47,22 @@ basicConfig(
 
 LOGGER = getLogger(__name__)
 
+CONFIG_FILE_URL= environ.get('CONFIG_FILE_URL')
+try:
+    if len(CONFIG_FILE_URL) == 0:
+        raise TypeError
+    try:
+        res = rget(CONFIG_FILE_URL)
+        if res.status_code == 200:
+            with open('config.env', 'wb+') as f:
+                f.write(res.content)
+        else:
+            log_error(f"Failed to download config.env {res.status_code}")
+    except Exception as e:
+        log_error(f"CONFIG_FILE_URL: {e}")
+except:
+    pass
+
 load_dotenv("config.env", override=True)
 
 Intervals = {"status": {}, "qb": "", "jd": "", "stopAll": False}
